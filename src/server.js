669371c0e04db0018ee88155403fe7ff3cb53b6d
d4g0 <tocarralero@gmail.com>
2021-01-15 13:41:35 -0500

    -

--- src/server.js
+++ src/server.js
@@ -6,14 +6,19 @@ import router from './router/router'
 const app = express()
 
 app.use(cors())
-process.env.NODE_ENV !== "production" && app.use(morgan("dev"))
+//
+// Debug
+//
+// process.env.NODE_ENV !== "production" && app.use(morgan("dev"))
+app.use(morgan("dev"))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
 app.use("/api", router)
 
-app.use("*", (req, res) => res.status(404).json({ error: `${req.originalUrl }` + "not found" }))
+app.use("*", (req, res) => res.status(404).json({ error: `route -> ${req.originalUrl} ` + " not found" }))
 
 
 export default app
