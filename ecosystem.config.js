e9937b607e03db31c9aedd36c2d10fbe7a4a72a1
d4g0 <tocarralero@gmail.com>
2021-01-15 03:36:34 -0500

    ecosyste

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+        {
+            name: 'railab.tech/api',
+            exec_mode: 'cluster',
+            instances: 'max',
+            script: './index.js',
+            env_production: {
+                "NODE_ENV": "production"
+            }
+        }
+    ]
+}
